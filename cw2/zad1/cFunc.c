7f4bb98188bd08210e76624a4a225d4e277ffede
Suselkowy <jakubsus77@gmail.com>
2023-07-13 16:33:01 -0400

    review cw2_zad1

--- cw2/zad1/cFunc.c
+++ cw2/zad1/cFunc.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+FILE* file_to_read;
+FILE* file_to_write;
+
 void close_file(FILE* f){
     fclose(f);
+}
+
+void close_files() {
+    close_file(file_to_read);
+    close_file(file_to_write);
+}
+
+void open_files(char* input_file, char* output_file){
+    file_to_read = fopen(input_file, "r");
+    if(file_to_read == NULL){
+        fprintf(stderr, "Failed to open first file");
+        exit(-1);
+    }
+
+    file_to_write = fopen(output_file, "w");
+    if(file_to_write == NULL){
+        fprintf(stderr, "Failed to open second file");
+        close_file(file_to_read);
+        exit(-1);
+    }
+}
+
+void perform_replacement(const char* letter_to_find, const char* letter_to_insert) {
+    char currLetter; 
+    while (fread(&currLetter, sizeof(char), 1, file_to_read) > 0)
+    {
+        if(currLetter == *letter_to_find){
+            currLetter = *letter_to_insert;
+        }
+        fwrite(&currLetter, sizeof(char), 1, file_to_write);
+    }
 }
 
 int main( int argc, char *argv[] )  {
@@ -12,31 +46,14 @@ int main( int argc, char *argv[] )  {
         return -1;
     }
     
-    FILE* file_to_read = fopen(argv[3], "r");
-    if(file_to_read == NULL){
-        fprintf(stderr, "Failed to open first file");
-        return -1;
-    }
+    const char* input_file = argv[3];
+    const char* output_file = argv[4];
+    open_files(input_file, output_file);
 
-    FILE* file_to_write = fopen(argv[4], "w");
-    if(file_to_write == NULL){
-        fprintf(stderr, "Failed to open second file");
-        close_file(file_to_read);
-        return -1;
-    }
+    const char* letter_to_find = argv[1];
+    const char* letter_to_insert = argv[2];
 
-    char currLetter; 
-    while (fread(&currLetter, sizeof(char), 1, file_to_read) > 0)
-    {
-        if(currLetter == *argv[1]){
-            currLetter = *argv[2];
-        }
-        fwrite(&currLetter, sizeof(char), 1, file_to_write);
-    }
-     
-    close_file(file_to_read);
-    close_file(file_to_write);
-
-
+    perform_replacement(letter_to_find, letter_to_insert);
+    close_files();
     return 0;
 }
--- cw2/zad1_wide-character/cFunc.c
+++ cw2/zad1_wide-character/cFunc.c
@@ -4,8 +4,52 @@
 #include <locale.h>
 #include <string.h>
 
+FILE* file_to_read;
+FILE* file_to_write;
+
 void close_file(FILE* f){
     fclose(f);
+}
+
+void handle_mbtowc_error(){
+    fprintf(stderr, "Invalid multibyte sequence");
+    close_files();
+    exit(-1);
+}
+
+void open_files(const char* input_file, const char* output_file) {
+    file_to_read = fopen(input_file, "r");
+    if (file_to_read == NULL){
+        fprintf(stderr, "Failed to open first file");
+        exit(-1);
+    }
+
+    file_to_write = fopen(output_file, "w");
+    if (file_to_write == NULL){
+        fprintf(stderr, "Failed to open second file");
+        close_file(file_to_read);
+        exit(-1);
+    }
+}
+
+void close_files() {
+    close_file(file_to_read);
+    close_file(file_to_write);
+}
+
+void perform_replacement(const wchar_t* letter_to_find, const wchar_t* letter_to_insert) {
+    wint_t currLetter;
+    while ((currLetter = fgetwc(file_to_read)) != WEOF) {
+        if(currLetter == *letter_to_find){
+            currLetter = *letter_to_insert;
+        }
+        fputwc(currLetter, file_to_write);
+    }
+}
+
+size_t multibyte_char_to_wint_t(wint_t* wint, char* multibyte_char){
+    size_t returnValue = mbtowc(&wint, multibyte_char, sizeof(wchar_t));
+    return returnValue;
 }
 
 int main( int argc, char *argv[] )  {
@@ -17,36 +61,23 @@ int main( int argc, char *argv[] )  {
         return -1;
     }
     
-    FILE* file_to_read = fopen(argv[3], "r");
-    if(file_to_read == NULL){
-        fprintf(stderr, "Failed to open first file");
-        return -1;
-    }
+    const char* input_file = argv[3];
+    const char* output_file = argv[4];
+    open_files(input_file, output_file);
 
-    FILE* file_to_write = fopen(argv[4], "w");
-    if(file_to_write == NULL){
-        fprintf(stderr, "Failed to open second file");
-        close_file(file_to_read);
-        return -1;
-    }
     wint_t letter_to_find;
-    mbtowc(&letter_to_find, argv[1], sizeof(wchar_t));
-
-    wint_t letter_to_insert = *argv[2];
-    mbtowc(&letter_to_insert, argv[2], sizeof(wchar_t));
-
-    wint_t currLetter;
-    while ((currLetter = fgetwc(file_to_read)) != WEOF)
-    {   
-        
-        if(currLetter == letter_to_find){
-            currLetter = letter_to_insert;
-        }
-        fputwc(currLetter, file_to_write);
+    if (multibyte_char_to_wint_t(&letter_to_find, argv[1]) == (size_t)-1) {
+        handle_mbtowc_error();
     }
+
+    wint_t letter_to_insert;
+    if (multibyte_char_to_wint_t(&letter_to_insert, argv[2]) == (size_t)-1) {
+        handle_mbtowc_error();
+    }
+
+    perform_replacement(&letter_to_find, &letter_to_insert);
      
-    close_file(file_to_read);
-    close_file(file_to_write);
+    close_files();
 
 
     return 0;
